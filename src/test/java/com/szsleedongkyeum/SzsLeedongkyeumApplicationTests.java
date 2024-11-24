6c96af97310f98c3de5fec07ca9efff572e048e0
kyeum_d <leedk1130@gmail.com>
2024-11-24 11:34:29 +0900

    Initial commit

---
+++ src/test/java/com/szsleedongkyeum/SzsLeedongkyeumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szsleedongkyeum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsLeedongkyeumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
