d2beb5c0d3d85da9cd7ecb6d8f6e2bf20f6b3797
kyeum_d <leedk1130@gmail.com>
2024-11-24 15:26:33 +0900

    feat(PE-2): Security config 수정

--- src/main/java/com/szsleedongkyeum/security/SecurityConfig.java
+++ src/main/java/com/szsleedongkyeum/security/SecurityConfig.java
@@ -1,15 +1,10 @@
 package com.szsleedongkyeum.security;
 
-import com.szsleedongkyeum.security.filter.RestAuthenticationFilter;
-import com.szsleedongkyeum.security.handler.RestAccessDeniedHandler;
-import com.szsleedongkyeum.security.handler.RestAuthenticationFailureHandler;
-import com.szsleedongkyeum.security.handler.RestAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.szsleedongkyeum.security.jwt.JwtAuthenticationFilter;
+import com.szsleedongkyeum.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,30 +16,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationProvider restAuthenticationProvider;
-    private final RestAuthenticationSuccessHandler restSuccessHandler;
-    private final RestAuthenticationFailureHandler restFailureHandler;
-    private final RestAccessDeniedHandler restAccessDeniedHandler;
-
-    public SecurityConfig(
-        @Qualifier("restAuthenticationProvider") AuthenticationProvider restAuthenticationProvider,
-        RestAuthenticationSuccessHandler restSuccessHandler,
-        RestAuthenticationFailureHandler restFailureHandler,
-        RestAccessDeniedHandler restAccessDeniedHandler) {
-        this.restAuthenticationProvider = restAuthenticationProvider;
-        this.restSuccessHandler = restSuccessHandler;
-        this.restFailureHandler = restFailureHandler;
-        this.restAccessDeniedHandler = restAccessDeniedHandler;
-    }
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(restAuthenticationProvider);
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
         http
             .securityMatcher("/**")
@@ -55,21 +33,10 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
-            .addFilterBefore(restAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
-            .authenticationManager(authenticationManager)
-            .exceptionHandling(exception -> exception
-                .accessDeniedHandler(restAccessDeniedHandler)
-            )
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
             .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
 
-    private RestAuthenticationFilter restAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
-        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter(http);
-        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
-        restAuthenticationFilter.setAuthenticationSuccessHandler(restSuccessHandler);
-        restAuthenticationFilter.setAuthenticationFailureHandler(restFailureHandler);
-        return restAuthenticationFilter;
-    }
 }
