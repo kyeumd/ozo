73514c64dc69dd862cfee0f9bf4e884642988cd4
kyeum_d <leedk1130@gmail.com>
2024-11-25 16:49:09 +0900

    refactor(PE-3): web client post api 요청 및 static 변경

--- src/main/java/com/szsleedongkyeum/utils/RestApiUtil.java
+++ src/main/java/com/szsleedongkyeum/utils/RestApiUtil.java
@@ -3,40 +3,40 @@ package com.szsleedongkyeum.utils;
 import static com.szsleedongkyeum.common.Error.ErrorCode.INTERNAL_API_SERVER_ERROR;
 
 import java.time.Duration;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Slf4j
-@RequiredArgsConstructor
 public class RestApiUtil<T> {
 
-    private final WebClient webClient;
+    private static final WebClient WEB_CLIENT = WebClient.builder().build();
 
-    public RestApiUtil(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder
-            .build();
+    private RestApiUtil() {
     }
 
-    public T fetchData(String endpoint, ParameterizedTypeReference<T> typeReference) {
-        return webClient.get()
-                        .uri(endpoint)
-                        .retrieve()
-                        .bodyToMono(typeReference) // 제네릭 타입으로 응답 처리
-                        .timeout(Duration.ofSeconds(20))
-                        .doOnSuccess(response -> log.info("API 호출 성공: {}", response))
-                        .doOnError(e -> {
-                            if (e instanceof WebClientResponseException ex) {
-                                log.error("API 호출 실패: {} - {}", ex.getStatusCode(), ex.getResponseBodyAsString());
-                            } else {
-                                log.error("API 호출 실패: {}", e.getMessage());
-                            }
-                        })
-                        .onErrorResume(e -> {
-                            throw new RuntimeException(INTERNAL_API_SERVER_ERROR.getMessage(), e);
-                        })
-                        .block();
+    public static <T, R> T postData(String endpoint, String apiKey, R requestBody, ParameterizedTypeReference<T> typeReference) {
+        return WEB_CLIENT.post()
+                         .uri(endpoint)
+                         .header("X-API-KEY", apiKey)
+                         .bodyValue(requestBody)
+                         .retrieve()
+                         .bodyToMono(typeReference)
+                         .timeout(Duration.ofSeconds(20))
+                         .doOnSuccess(response -> log.info("API 호출 성공: {}", response))
+                         .doOnError(e -> {
+                                 if (e instanceof WebClientResponseException ex) {
+                                     log.error("API 호출 실패: {} - {}", ex.getStatusCode(), ex.getResponseBodyAsString());
+                                 } else {
+                                     log.error("API 호출 실패: {}", e.getMessage());
+                                 }
+                             }
+                         )
+                         .onErrorResume(e -> {
+                             throw new RuntimeException(INTERNAL_API_SERVER_ERROR.getMessage(), e);
+                         })
+                         .block();
     }
+
 }
