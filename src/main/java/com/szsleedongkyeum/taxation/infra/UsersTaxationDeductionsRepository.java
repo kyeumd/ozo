6faf147536f37937d14ad82ffb2159f53fb51718
kyeum_d <leedk1130@gmail.com>
2024-11-27 14:11:05 +0900

    refactor(PE-4): user taxation 정보 통합

---
+++ src/main/java/com/szsleedongkyeum/taxation/infra/UsersTaxationDeductionsRepository.java
@@ -0,0 +1,10 @@
+package com.szsleedongkyeum.taxation.infra;
+
+import com.szsleedongkyeum.taxation.model.domain.UsersTaxationDeductions;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersTaxationDeductionsRepository extends JpaRepository<UsersTaxationDeductions, Long> {
+
+    List<UsersTaxationDeductions> findAllByUserId(Long userId);
+}
