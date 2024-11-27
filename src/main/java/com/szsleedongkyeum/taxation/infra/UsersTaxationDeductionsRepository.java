8ee8da6f49dc1658c57d0a02be840769c58d7359
kyeum_d <leedk1130@gmail.com>
2024-11-27 14:58:49 +0900

    feat(PE-5): User Taxation Deduction year 기준 찾기 추가

--- src/main/java/com/szsleedongkyeum/taxation/infra/UsersTaxationDeductionsRepository.java
+++ src/main/java/com/szsleedongkyeum/taxation/infra/UsersTaxationDeductionsRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsersTaxationDeductionsRepository extends JpaRepository<UsersTaxationDeductions, Long> {
 
     List<UsersTaxationDeductions> findAllByUserId(Long userId);
+
+    List<UsersTaxationDeductions> findAllByUserIdAndDeductionYear(Long userId, int deductionYear);
 }
