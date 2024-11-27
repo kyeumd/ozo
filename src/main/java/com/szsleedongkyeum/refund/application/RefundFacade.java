3deaeac5a65a5ad594a72310a6a20be0cc16f34d
kyeum_d <leedk1130@gmail.com>
2024-11-27 15:40:17 +0900

    feat(PE-5): 산출세액 계산 추가

---
+++ src/main/java/com/szsleedongkyeum/refund/application/RefundFacade.java
@@ -0,0 +1,24 @@
+package com.szsleedongkyeum.refund.application;
+
+import com.szsleedongkyeum.refund.model.service.RefundService;
+import com.szsleedongkyeum.security.service.AuthenticationUserService;
+import com.szsleedongkyeum.taxation.model.service.UserTaxationQueryService;
+import com.szsleedongkyeum.taxation.model.service.out.UsersTaxationQueryResult;
+import java.math.BigDecimal;
+import java.time.Year;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefundFacade {
+
+    private final UserTaxationQueryService userTaxationQueryService;
+    private final RefundService refundService;
+
+    public BigDecimal calculateRefund() {
+        Long userId = AuthenticationUserService.getUserId();
+        UsersTaxationQueryResult userTaxation = userTaxationQueryService.getUserTaxation(userId, Year.now().getValue());
+        return refundService.calculateRefund(userTaxation.totalIncome(), userTaxation.deductionAmount(), userTaxation.taxCreditsAmount());
+    }
+}
--- src/main/java/com/szsleedongkyeum/refund/model/service/RefundService.java
+++ src/main/java/com/szsleedongkyeum/refund/model/service/RefundService.java
@@ -7,8 +7,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class RefundService {
 
-    public BigDecimal calculateRefund(BigDecimal taxableIncome) {
+    public BigDecimal calculateRefund(BigDecimal taxableIncome, BigDecimal deductionAmount, BigDecimal taxCreditsAmount) {
+        //과세 표준 계산
+        BigDecimal taxBase = taxCreditsAmount.subtract(deductionAmount);
+
+        //산출세액 계산
         TaxBracket bracket = TaxBracket.findBracket(taxableIncome);
-        return bracket.calculateTax(taxableIncome);
+        BigDecimal calculateTax = bracket.calculateTax(taxableIncome);
+
+        //결정세액 계산
+        return calculateTax.subtract(taxCreditsAmount);
     }
 }
